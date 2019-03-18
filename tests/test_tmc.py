616a9363c6babd12ca98b7f3ea8cad63b7f00ce0
Nick Tucker <nicktucker4@gmail.com>
2019-03-18 19:06:01 -0400

    Added test for ForumScraper.scrape_posts_from_thread

--- tests/test_tmc.py
+++ tests/test_tmc.py
@@ -4,7 +4,7 @@
 """Tests for `tmc` package."""
 
 from bs4 import BeautifulSoup
-from tmc.forum_scraper import User, Post
+from tmc.forum_scraper import User, Post, ForumScraper
 import pytest
 
 def test_user_collection():
@@ -27,3 +27,9 @@ def test_media_post_collect():
     assert p.media == 'https://www.youtube.com/embed/IsO3QiCR6_g?wmode=opaque'
     assert p.likes == 10
     assert p.loves == 1
+
+def test_scrape_posts_from_thread():
+    forum_scraper = ForumScraper()
+
+    posts = forum_scraper.scrape_posts_from_thread(url='https://teslamotorsclub.com/tmc/threads/i-thought-i-would-mention-that-i-think-tesla-has-one-of-the-nicest-websites.14/')
+    assert len(posts) == 4
