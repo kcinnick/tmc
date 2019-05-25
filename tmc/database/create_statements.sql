483f9114d235bb55c6a5de3e84c4c2050b58dc4c
Nick Tucker <nicktucker4@gmail.com>
2019-05-25 18:35:23 -0400

    Updated create_statements.sql

--- tmc/database/create_statements.sql
+++ tmc/database/create_statements.sql
@@ -2,6 +2,7 @@ CREATE DATABASE tmc;
 USE tmc;
 CREATE TABLE `posts` (
 	`id` INT NOT NULL UNIQUE,
+	`thread_title` VARCHAR(200),
 	`username` VARCHAR(90) NOT NULL,
 	`posted_at` DATETIME NOT NULL,
 	`message` TEXT,
