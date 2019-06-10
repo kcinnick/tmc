cebe3d07f0a31d2e18895a39217ed7c80cad582c
Nick Tucker <nicktucker4@gmail.com>
2019-06-10 19:21:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['bs4', 'requests', 'pymysql']
+requirements = ['bs4', 'requests', 'pymysql', 'pandas']
 
 setup_requirements = ['pytest-runner', ]
 
