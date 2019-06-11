cced3416da94389c027ab33e5000304695f9a31a
Nick Tucker <nicktucker4@gmail.com>
2019-06-11 15:10:56 -0400

    Removed User logic.

--- tmc/forum_scraper.py
+++ tmc/forum_scraper.py
@@ -8,31 +8,6 @@ import requests
 import re
 
 
-class User:
-    def __init__(self):
-        self.username = None
-        self.member_type = None
-        self.joined = None
-        self.messages = None
-        self.location = None
-
-    def get_info(self, post: Tag = None, url: str = None):
-        """
-        Retrieves basic user information.
-        TODO: Add logic for parsing from URL.
-        """
-
-        if post:
-            user_info = post.find('div', class_='messageUserInfo')
-            self.username = user_info.find('a', class_='username').text
-            self.member_type = user_info.find('em', class_='userTitle').text
-
-            extra_user_info = {i.text[:-1]: i.find_next('dd').text.strip() for i in post.find_all('dt')}
-            self.joined = extra_user_info['Joined']
-            self.messages = ([], extra_user_info['Messages'])
-            self.location = extra_user_info['Location']
-
-
 class ForumScraper:
     def __init__(self):
         self.session = requests.Session()
